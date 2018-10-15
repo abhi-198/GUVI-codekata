dbb59cf9a1c1e921e53209265cfbb5a08e59f282
abhi-198 <43150958+abhi-198@users.noreply.github.com>
2018-10-16 00:42:23 +0530

    Find Greater of three values

---
+++ isGreater.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    long long int a,b,c;
+    scanf("%lld%lld%lld",&a,&b,&c);
+    if(a>b)
+    {
+        if(a>c)
+        {
+            printf("%lld",a);
+        }
+        else
+        {
+            printf("%lld",c);
+        }
+    }
+    else
+    {
+        if(b>c)
+        {
+            printf("%lld",b);
+        }
+        else
+        {
+            printf("%lld",c);
+        }
+    }
+    return 0;
+}
